b22968256c4c4efab22d4d5e5109b33d7a9e4ac4
Anderson Franco <andersonsfj@al.insper.edu.br>
2023-11-12 22:47:45 -0300

    alteracao campus mobile

--- js/perfil_match.js
+++ js/perfil_match.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
     nome = this.querySelector('.nome')
-    nome.innerHTML = localStorage.getItem('nome')
+    nome.innerHTML = 'Antonio'
     foto = this.querySelector('.img-match')
-    imagem = localStorage.getItem('foto').toString()
-    foto.setAttribute('src', imagem)
 })
